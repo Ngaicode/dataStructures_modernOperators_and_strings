9a05438305a9b826a9d6fbc6ecce0be188880042
Ngaicode <mjombakevin376@gmail.com>
2022-09-19 20:53:17 +0300

    learnt about logical assignment operators and did a challenge

--- starter/script.js
+++ starter/script.js
@@ -73,40 +73,40 @@ restaurant.orderDelivery({
 // we mostly use the spread operator in cases where we'd require multiple values separated by commas eg in an array litteral or when passing arguments to a function
 // we can only use the spread operator when building an array or when passsing values to a function
 // we use the spread operator in places where we want an ordered list of values separated by commas(WHEN WE WANT AN ARRAY)
-const arr = [5, 6, 7];
-console.log(...arr);
-const numb = [2, 3, 4, ...arr];
-console.log(numb);
-
+// const arr = [5, 6, 7];
+// console.log(...arr);
+// const numb = [2, 3, 4, ...arr];
+// console.log(numb);
+//
 // a more practical example of array destructuring
-const menu = [...restaurant.mainMenu, "ugali", "skuma"];
-console.log(menu);
-
+// const menu = [...restaurant.mainMenu, "ugali", "skuma"];
+// console.log(menu);
+//
 // one of the most typical use case of the spread operator is to create shallow copies of arrays and also to merge separate arrays into one array
 
 // 1.creating shallow copies of array
 // not a copy but just a different variable pointing to the same object(arrays are objects) in memory
 const relatedCopy = restaurant.mainMenu;
 // proof that this is just a refrence to the same object
-console.log(relatedCopy === restaurant.mainMenu);
+// console.log(relatedCopy === restaurant.mainMenu);
 
 // the spread operator creates a shallow copy of the array and doesn't refer to the same object in memory
 const trueUnrelatedCopy = [...restaurant.mainMenu];
-console.log(trueUnrelatedCopy);
-// prooof that this is a shallow copy and not the same array(object)
-console.log(trueUnrelatedCopy === restaurant.mainMenu);
-
+// console.log(trueUnrelatedCopy);
+// // prooof that this is a shallow copy and not the same array(object)
+// console.log(trueUnrelatedCopy === restaurant.mainMenu);
+//
 // 2. joining arrays into one
 const fullMenu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-console.log(fullMenu);
+// console.log(fullMenu);
 
 // iterables are : strings ,arrays,maps, sets .NOT objects
 // 1.using the spread operator on  a string
-const str = "Mjomba";
-console.log(...str);
-const nameArr = [...str, , "s"];
-console.log(nameArr);
-
+// const str = "Mjomba";
+// console.log(...str);
+// const nameArr = [...str, , "s"];
+// console.log(nameArr);
+//
 // real world example of using the spread operator to pass arguments to functions
 // const ingredients = [
 //   prompt(`let's make pasta: Ingredient 1 ?`),
@@ -121,7 +121,7 @@ console.log(nameArr);
 // Objects
 // we can use the spread operator on objects to create copies of the object
 const newRestaurant = { foundedIn: 2000, ...restaurant, founder: "mjomba" };
-console.log(newRestaurant);
+// console.log(newRestaurant);
 
 // // destructuring objects
 // // when destructuring objects ,we have to provide the variable names that ecactly match the property names of the vlues we are trying to unpack from an object
@@ -226,7 +226,7 @@ console.log(newRestaurant);
 
 //SPREAD bcz its on the right hand side of the assignment operator
 const arr2 = [1, 2, ...[3, 4]];
-console.log(arr2);
+// console.log(arr2);
 
 //rest synatx because it's to the left of the assignment opearator
 const [a, b, ...others] = [1, 2, 3, 4, 5];
@@ -238,7 +238,7 @@ const [pizza, , risotto, ...other] = [
   ...restaurant.mainMenu,
   ...restaurant.starterMenu,
 ];
-console.log(pizza, risotto, other);
+// console.log(pizza, risotto, other);
 //while destructuring,the rest synatx shouild always come last
 //the rest operator will not include any skipped over variables/values ,it will always include the REST of the elements that are left after destructuring
 
@@ -252,15 +252,15 @@ console.log(weekdays);
 //the rest operator will take multiple values and packs them into an array/objects
 //such a function can take any number of arbitrary arguments and still work perfectly fine
 //this is because ,no matter how many arguments we pass when calling the functions ,they'll just be packed into an array  by the rest operator ,and we can work on that array
-const add = function (...numbers) {
-  console.log(numbers);
-  let summ = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    summ += numbers[i];
-  }
-  console.log(summ);
-};
-
+// const add = function (...numbers) {
+//   console.log(numbers);
+//   let summ = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     summ += numbers[i];
+//   }
+//   console.log(summ);
+// };
+//
 // add(2, 3);
 // add(4, 5, 6, 7);
 // add(12, 13, 14, 15);
@@ -280,36 +280,36 @@ const add = function (...numbers) {
 //
 
 //SHART CIRCUITING
-console.log("-----short circuiting using the or operator-------");
+// console.log("-----short circuiting using the or operator-------");
 //the logical or || oerrator
 //in the case of the logical or operator,it returns the first truthy values in the evaluation and shorts circuits the  rest of the evaluation
 //in the case where  all values in the evaluation are true ,it'll return the first value
 //in the case where all values are falsey,it;l return the last falsey value
-console.log(2 || "Mjomba");
-console.log("" || 23);
-console.log(true || 0);
-console.log(undefined || null);
-console.log(0 || undefined || "" || false);
-
+// console.log(2 || "Mjomba");
+// console.log("" || 23);
+// console.log(true || 0);
+// console.log(undefined || null);
+// console.log(0 || undefined || "" || false);
+//
 //in the case of the logical or operator,it returns the first truthy values in the evaluation and shorts circuits the  rest of the evaluation
-console.log(undefined || 0 || "" || "hello" || 23);
+// console.log(undefined || 0 || "" || "hello" || 23);
 // a more practical application
 // restaurant.numGuests = 35;
 const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
 //doing the same thing but using the short circuit evaluation of the or || operator
 // we can use the or operator to set default values if the first operand is falsey
 const guest2 = restaurant.numGuests || 23;
-console.log(guest2);
-
-console.log(guests1);
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-console.log("short circuiting using the and && opearator----");
+// console.log(guest2);
+//
+// console.log(guests1);
+// //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+// console.log("short circuiting using the and && opearator----");
 //the logical and opearator returns the first falsey values and exits evaluation
 //in the case where all values are truthy,it'll return the last truthy value in the evaluation
-console.log(0 && "meow");
-console.log("cat" && "meow" && "purr");
-console.log("7" && 7 && 0 && "woof");
-
+// console.log(0 && "meow");
+// console.log("cat" && "meow" && "purr");
+// console.log("7" && 7 && 0 && "woof");
+//
 // a more practical example
 if (restaurant.orderPizza) {
   restaurant.orderPizza("mushroom", "olives", "ham");
@@ -325,11 +325,131 @@ restaurant.orderPizza && restaurant.orderPizza("njahi", "nduma");
 //the evaluation will only continue if the first value was nullish or undefined ,or else it'll just return the first  non-nullish value and short circuit the evaluation
 //so in this case ,even though though 0 is falsey,it isn't nullish so it won't short circuit the evaluation
 const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
-console.log(1 ?? undefined);
-console.log(2 ?? null);
-console.log(1 ?? 2 ?? 3);
-//in the case where all values are nullish ,it'll return the last nullish value
-console.log(null ?? undefined);
+// console.log(guestCorrect);
+// console.log(1 ?? undefined);
+// console.log(2 ?? null);
+// console.log(1 ?? 2 ?? 3);
+// //in the case where all values are nullish ,it'll return the last nullish value
+// console.log(null ?? undefined);
 //the evaluation will only continue if the first value was nullish or undefined ,or else it'll just return the first  NON-NULLISH value and short circuit the evaluation
 console.log(null ?? "Meow");
+
+//logical assignment operators
+
+const rest1 = {
+  name: "hoteli",
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: "hoteli",
+  owner: "Ruth Mwathi",
+};
+
+//the or  operator works by returning the first truthy value and short circuiting the rest of the evaluation
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests = rest1.numGuests || 10;
+//
+//the logical or || assignment operator
+//this operator assigns a value to a variable if that variable is currently falsey
+// rest2.numGuests ||= 10;
+// rest1.numGuests ||= 10;
+//
+
+//LOGICAL NULLISH ASSIGNMENT operator
+//the nullish assignment operator will assign  a value to a variable if the variable is currently nullish
+//if the variable is not nullish ,it will simply retain it's value
+rest2.numGuests ??= 10;
+rest1.numGuests ??= 10;
+
+console.log(rest2, rest1);
+
+//LOGICAL AND ASSIGNMENT OPERATOR
+// rest1.owner = rest1.owner && "shddjahkhdh";
+// rest2.owner = rest2.owner && "hsjhajdajskjask";
+//
+rest1.owner &&= "ddsfsfsfsfs";
+rest2.owner &&= "jjagdjagdjdgdg";
+
+console.log(rest2, rest1);
+
+//coding challenge 16
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//1. Create one player array for each team (variables 'players1' and 'players2')
+const [player1, player2] = game.players;
+console.log(player1);
+// const player2 = game.players[1];
+console.log(player2);
+
+//2. The first player in any player array is the goalkeeper and the others are fieldplayers. For Bayern Munich (team 1) create one variable ('gk') with thegoalkeeper's name, and one array ('fieldPlayers') with all the remaining 10field players
+
+const [gk, ...fieldPlayers] = player1;
+console.log(gk, fieldPlayers);
+
+//3. Create an array 'allPlayers' containing all players of both teams (22players)
+const allPlayers = [...player1, ...player2];
+console.log(allPlayers);
+
+//4.During the game, Bayern Munich (team 1) used 3 substitute players. So create anew array ('players1Final') containing all the original team1 players plus'Thiago', 'Coutinho' and 'Perisic'
+const players1Final = [...player1, "Thiago", "Coutinho", "Perisic"];
+console.log(players1Final);
+
+//5.Based on the game.odds object, create one variable for each odd (called'team1', 'draw' and 'team2')
+console.log(game.odds);
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, team2, draw);
+
+//6.Write a function ('printGoals') that receives an arbitrary number of playernames (not an array) and prints each of them to the console, along with thenumber of goals that were scored in total (number of player names passed in)
+function printGoals(...playernames) {
+  for (const player of playernames) {
+    console.log(
+      `${player} scored a goal,a total of  ${playernames.length} goals were scored in that match`
+    );
+  }
+}
+printGoals("Ruth", "Mwathi", "Gideon");
+printGoals(...game.scored);
+
+//The team with the lower odd is more likely to win. Print to the console whichteam is more likely to win, without using an if/else statement or the ternaryoperator.
+team1 < team2 && console.log("team1 is more likely to win");
